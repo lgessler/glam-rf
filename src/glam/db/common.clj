5b11272d07be65d921132cdb4b55c8c9e025899b
Luke Gessler <ldg3fa@virginia.edu>
2019-03-14 14:46:46 -0400

    add docstring to def-with-spec

--- src/glam/db/common.clj
+++ src/glam/db/common.clj
@@ -1,7 +1,10 @@
 (ns glam.db.common
-  (:require [cljs.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]))
 
 (defmacro def-with-spec
+  "A combination of def and clojure.spec.alpha/def: defines a new var from a
+  given symbol, name, with the value ::name, and also creates a spec def using
+  pred."
   [name pred]
   (let [kwd (keyword (str *ns*) (str name))]
     `(do
