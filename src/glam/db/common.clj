9ae1a06419a8c3a613802f11620129f9ce7ab4c4
Luke Gessler <ldg3fa@virginia.edu>
2019-06-24 11:24:20 -0500

    Make defdbkwd use un-namespaced keywords in the rf db

--- src/glam/db/common.clj
+++ src/glam/db/common.clj
@@ -6,12 +6,14 @@
   symbol, name, with the value ::name, and also creates a spec def using pred.
   If pred is not provided, assumes the spec should check for the keyword."
   ([name]
-   (let [kwd (keyword (str *ns*) (str name))]
+   (let [kwd (keyword #_(str *ns*) (str name))
+         ns-kwd (keyword (str *ns*) (str name))]
      `(do
         (def ~name ~kwd)
-        (s/def ~kwd #(= % ~kwd)))))
+        (s/def ~ns-kwd #(= % ~kwd)))))
   ([name pred]
-   (let [kwd (keyword (str *ns*) (str name))]
+   (let [kwd (keyword #_(str *ns*) (str name))
+         ns-kwd (keyword (str *ns*) (str name))]
      `(do
         (def ~name ~kwd)
-        (s/def ~kwd ~pred)))))
+        (s/def ~ns-kwd ~pred)))))
